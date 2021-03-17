cb68fa97f17b6b11b578ae36fb3621c87139474a
john <john@john.com>
2021-03-17 23:55:00 +0800

    提交

--- src/main/java/leetcode/S0121/Solution.java
+++ src/main/java/leetcode/S0121/Solution.java
@@ -12,4 +12,22 @@ package leetcode.S0121;
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max =0;
+        for(int i=0;i<prices.length;i++){
+            if(prices[i] < min ){
+                min = prices[i];
+            }
+            max =Math.max(max,prices[i]-min);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+
+        int[] prices= {7,1,5,3,6,4};
+        int maxProfit = new Solution().maxProfit(prices);
+        System.out.println(maxProfit);
+    }
 }
